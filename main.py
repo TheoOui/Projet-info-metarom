b5d314761530cb0ded2bb418a43dabfdde0f8c75
TheoOui <theoaz25@gmail.com>
2022-03-06 21:58:12 +0100

    Fixup
    Ajout print

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 print("Hi,Theo")
 
 print("Changement 1")
-
+print ("bonsoir Monsieur ")
  # https://www.youtube.com/watch?v=JJjTByHN2iI
  # https://www.youtube.com/watch?v=lwlg5wAhpPk
  #https://github.com/ktbyers/netmiko
